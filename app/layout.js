97fb3cd62a0beebd9bed872e8e48f45fc3f6bce2
Hasan <mailin@gmail.com>
2026-04-26 18:13:02 +0300

    google verification fix

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: 'MidyePro ',
+  title: 'MidyePro',
   description: 'Midye çiftliği yönetim ve takip sistemi',
+  verification: {
+    google: 'r_3TCbMcb5kO47WStkWbXL0zs9cKmOlz0p_LotTPgVw'
+  }
 };
-
 export default function RootLayout({ children }) {
   return (
     <html
